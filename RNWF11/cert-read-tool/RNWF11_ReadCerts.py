b1a3ab71e1c0b44c8031aca826915825f1d839eb
Randy Wu - C14166 <randywu763@gmail.com>
2024-01-02 13:50:06 -0800

    Update main read cert script

--- RNWF11/cert-read-tool/RNWF11_ReadCerts.py
+++ RNWF11/cert-read-tool/RNWF11_ReadCerts.py
@@ -1,8 +1,17 @@
 import subprocess
+ 
+def find_python(name):
+  from shutil import which
+  return which(name) is not None
 
-subprocess.run('python RNWF11_ReadCert_Signer.py', shell=True, check=False)
-subprocess.run('python RNWF11_ReadCert_Client.py', shell=True, check=False)
-
-subprocess.run('python3 RNWF11_ReadCert_Signer.py', shell=True, check=False)
-subprocess.run('python3 RNWF11_ReadCert_Client.py', shell=True, check=False)
-
+print("\nLooking for the Python executable...")
+if find_python("python3.exe") | find_python("python3"):
+  print("The 'python3' executable was found")
+  subprocess.run('python3 RNWF11_ReadCert_Signer.py', shell=True, check=False)
+  subprocess.run('python3 RNWF11_ReadCert_Client.py', shell=True, check=False)
+elif find_python("python.exe") | find_python("python"):
+  print("The 'python' executable was found")
+  subprocess.run('python RNWF11_ReadCert_Signer.py', shell=True, check=False)
+  subprocess.run('python RNWF11_ReadCert_Client.py', shell=True, check=False)
+else:
+  print("Neither 'python' nor 'python3' was found as a valid executable in the environment's path")
